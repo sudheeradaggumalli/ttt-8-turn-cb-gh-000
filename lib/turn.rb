40d2de674dd49d27af9e6bab92e0cd48ba8c71bd
Sudheera Daggumalli <sudheeradaggumalli@gmail.com>
2017-09-06 17:57:17 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -43,3 +43,19 @@ end
 def move(board,index,value='X')
     board[index]=value
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  value=gets.strip
+  index=input_to_index(value)
+  until valid_move?(board,index)
+      if valid_move?(board,index)
+         move(board,index,value='X')
+         display_board(move)
+      else
+        puts "Invalid position reenter the value"
+        value=gets.strip
+        index=input_to_index(value)
+      end
+  end
+end
